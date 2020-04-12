d4b50f9abb0adf4fba44d500fe711a468fd86c00
cskim13 <cskim131313@gmail.com>
2020-04-12 22:45:38 +0300

    Added Mortgage Calculator for lab2

---
+++ modules/mortgage-calculator/include/MortgageCalc.h
@@ -0,0 +1,36 @@
+// Copyright 2020 Petrov Anton
+
+#ifndef MODULES_MORTGAGE_CALCULATOR_INCLUDE_MORTGAGECALC_H_
+#define MODULES_MORTGAGE_CALCULATOR_INCLUDE_MORTGAGECALC_H_
+
+class MortgageCalc {
+	
+	int cost;
+	int downPayment;
+	int creditTerm;
+	double interestRate;
+	int totalFee; // итогова€ плата
+	double monthlyPayment; //ежемес€чный платЄж
+	double monthlyRate;// ежемес€чный процент
+public:
+	MortgageCalc();
+	MortgageCalc(int _cost, int _downPayment, int _creditTerm, double _interestRate);
+	
+	void calc();
+
+	void setCost(const int& cost);
+	void setDownPayment(const int& cost);
+	void setCreditTerm(const int& cost);
+	void setInterestRate(const double& cost);
+
+	int getCost();
+	int getDownPayment();
+	int getCreditTerm();
+	double getInterestRate();
+	int getTotalFee();
+	double getMonthlyRate();
+	int getMonthlyPayment();
+
+};
+
+#endif  // MODULES_MORTGAGE_CALCULATOR_INCLUDE_MORTGAGECALC_H_
---
+++ modules/mortgage-calculator/test/test_mortgage_calculator.cpp
@@ -0,0 +1,4 @@
+// Copyright 2020 Petrov Anton
+
+#include "include/MortgageCalc.h"
+#include <gtest/gtest.h>
