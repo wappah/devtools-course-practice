6d038210116602cbaa3b79780204d1a89f1ff8d3
Zinkov Artem <zonckov.artyom@yandex.ru>
2020-05-08 18:31:58 +0300

    include

--- modules/odd_even_batcher_merge/application/merge-batcher-app.cpp
+++ modules/odd_even_batcher_merge/application/merge-batcher-app.cpp
@@ -1,7 +1,6 @@
 // Copyright 2020 Zinkov Artem
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <iostream>
 #include <string>
 
 #include "include/merge_batcher_app.h"
@@ -9,6 +8,6 @@
 int main(int argc, const char** argv) {
     Merge_Batcher_App app;
     std::string output = app(argc, argv);
-    printf("%s\n", output.c_str());
+    std::cout << output << std::endl;
     return 0;
 }
--- modules/odd_even_batcher_merge/src/merge_batcher_app.cpp
+++ modules/odd_even_batcher_merge/src/merge_batcher_app.cpp
@@ -1,9 +1,5 @@
 // Copyright 2020 Zinkov Artem
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
 #include <string>
 #include <sstream>
 #include <algorithm>
