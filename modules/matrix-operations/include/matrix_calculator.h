49bac1e2111cbd0da1005be8abf8e534b2ec6ac1
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-05-12 00:23:19 +0300

    codestyle

--- modules/matrix-operations/include/matrix_calculator.h
+++ modules/matrix-operations/include/matrix_calculator.h
@@ -6,16 +6,16 @@
 
 class MatrixCalculator {
  public:
-   MatrixCalculator();
-   std::string operator()(int argc, const char** argv);
+  MatrixCalculator();
+  std::string operator()(int argc, const char** argv);
 
  private:
-   void help(const char* appname, const char* message = "");
-   bool validateNumberOfArguments(int argc, const char** argv);
-   std::string message_;
-   typedef struct {
-     double _zero;
-   } Arguments;
+  void help(const char* appname, const char* message = "");
+  bool validateNumberOfArguments(int argc, const char** argv);
+  std::string message_;
+  typedef struct {
+    double _zero;
+  } Arguments;
 };
 
 #endif  // MODULES_MATRIX_OPERATIONS_INCLUDE_MATRIX_OPERATIONS_APP_H_
