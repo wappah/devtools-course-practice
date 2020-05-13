d7af9591a8633698bfb324d827590f20fb7effde
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-05-13 13:45:47 +0300

    application main file

--- modules/matrix-operations/application/matrix_calculator.cpp
+++ modules/matrix-operations/application/matrix_calculator.cpp
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string>
 
-// #include "include/complex_calculator.h"
+ #include "include/matrix_calculator.h"
 
 int main(int argc, const char** argv) {
-    /*ComplexCalculator app;
+    MatrixCalculator app;
     std::string output = app(argc, argv);
-    printf("%s\n", output.c_str());*/
+    printf("%s\n", output.c_str());
     return 0;
 }
