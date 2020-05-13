95c18ea4c01cfeb98e2085760723ec142283c590
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-05-13 14:00:34 +0300

    add testing class

--- modules/matrix-operations/test/test_matrix_calculator.cpp
+++ modules/matrix-operations/test/test_matrix_calculator.cpp
@@ -1,7 +1,36 @@
 // Copyright 2020 Golubev Vladislav
 
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
 #include "include/matrix_calculator.h"
+
+using ::testing::internal::RE;
+
+class MatrixCalculatorTest : public ::testing::Test {
+ protected:
+  void Act(std::vector<std::string> args_) {
+    std::vector<const char*> options;
+
+    options.push_back("MatrixCalculator");
+    for (std::size_t i = 0; i < args_.size(); ++i) {
+      options.push_back(args_[i].c_str());
+    }
+
+    const char** argv = &options.front();
+    int argc = static_cast<int>(args_.size()) + 1;
+
+    output_ = app_(argc, argv);
+  }
+
+  void Assert(std::string expected) {
+    EXPECT_TRUE(RE::PartialMatch(output_, RE(expected)));
+  }
+
+private:
+  MatrixCalculator app_;
+  std::string output_;
+};
 
 TEST(MatrixOperationsAppTest, test) {
   ASSERT_EQ(0, 0);
